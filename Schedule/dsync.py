33df2ad6730c9e9aa6d78637dac1417c0c96cbf8
spudguy77 <theressistance1@gmail.com>
2018-04-06 16:59:37 -0500

    completed data sync function

--- Schedule/dsync.py
+++ Schedule/dsync.py
@@ -4,15 +4,17 @@ import io
 import csv
 
 
-def download():
-    # change group to CS for complete schedule
+# download the new course csv
+def dataSync():
+    # change group to CS for complete schedule, CompSci for partial schedule
     p = {'Description': 'yes', 'Group': 'CS', 'Semester': '1188'}
     sess = requests.session()
     info = sess.post('https://rabi.phys.virginia.edu/mySIS/CS2/deliverData.php', p)
     data = pandas.read_csv(io.StringIO(info.text, "\n"), header=0)
     data.to_csv('lousdata.csv')
-
     print "data sync complete"
 
 
-download()
+# next step?
+
+dataSync()
