0263a7b16ce81d036b5bd5ba44fb19ee46a47bfa
spudguy77 <theressistance1@gmail.com>
2018-04-06 15:30:41 -0500

    test commit

---
+++ Schedule/dsync.py
@@ -0,0 +1,20 @@
+import requests
+import pandas as p
+import io
+import csv
+
+
+def download():
+    # change group to CS for complete schedule
+    p = {'Description': 'yes', 'Group': 'CompSci', 'Semester': '1188'}
+    sess = requests.session()
+    info = sess.post('https://rabi.phys.virginia.edu/mySIS/CS2/deliverData.php', p)
+    data = p.read_csv(io.StringIO(info.text, "\n"), header=0)
+    # file = open("data.txt", "w")
+    # file.write(info.text.encode('utf-8')) #
+    # print info.text
+
+    print "finished"
+
+
+download()
