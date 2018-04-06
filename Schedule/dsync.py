be78f5800a0875a6efc4b45a4d2d466c1df75b22
spudguy77 <theressistance1@gmail.com>
2018-04-06 16:53:52 -0500

    resolved utf-8 errors

--- Schedule/dsync.py
+++ Schedule/dsync.py
@@ -6,15 +6,13 @@ import csv
 
 def download():
     # change group to CS for complete schedule
-    p = {'Description': 'yes', 'Group': 'CompSci', 'Semester': '1188'}
+    p = {'Description': 'yes', 'Group': 'CS', 'Semester': '1188'}
     sess = requests.session()
     info = sess.post('https://rabi.phys.virginia.edu/mySIS/CS2/deliverData.php', p)
-    data = pandas.read_csv(io.StringIO(info.text, "\n"), header=0, encoding='utf-8')
-    # file = open("data.txt", "w")
-    # file.write(info.text.encode('utf-8'))
-    # print info.text
+    data = pandas.read_csv(io.StringIO(info.text, "\n"), header=0)
+    data.to_csv('lousdata.csv')
 
-    print "finished"
+    print "data sync complete"
 
 
 download()
