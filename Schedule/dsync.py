34834df421af676a668b4c3f98bcd6981f9e3e0c
spudguy77 <theressistance1@gmail.com>
2018-04-06 16:39:07 -0500

    added fetch encoding

--- Schedule/dsync.py
+++ Schedule/dsync.py
@@ -3,14 +3,13 @@ import pandas
 import io
 import csv
 
-#New comment!
 
 def download():
     # change group to CS for complete schedule
     p = {'Description': 'yes', 'Group': 'CompSci', 'Semester': '1188'}
     sess = requests.session()
     info = sess.post('https://rabi.phys.virginia.edu/mySIS/CS2/deliverData.php', p)
-    data = pandas.read_csv(io.StringIO(info.text, "\n"), header=0)
+    data = pandas.read_csv(io.StringIO(info.text, "\n"), header=0, encoding='utf-8')
     # file = open("data.txt", "w")
     # file.write(info.text.encode('utf-8'))
     # print info.text
